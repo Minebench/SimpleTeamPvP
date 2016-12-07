91d8e36770253de8f61f976a9a0155abaf3a9f1b
Max Lee <mail@moep.tv>
2016-12-07 22:53:05 +0100

    Catch all items when no durability is configured

--- src/main/java/de/themoep/simpleteampvp/games/SimpleTeamPvPGame.java
+++ src/main/java/de/themoep/simpleteampvp/games/SimpleTeamPvPGame.java
@@ -884,9 +884,7 @@ public abstract class SimpleTeamPvPGame implements Listener {
     }
 
     public boolean isDrop(ItemStack item) {
-        return item.getDurability() == 0
-                ? getDrops().contains(item.getType().toString())
-                : getDrops().contains(item.getType().toString() + ":" + item.getDurability());
+        return getDrops().contains(item.getType().toString()) || getDrops().contains(item.getType().toString() + ":" + item.getDurability());
     }
 
     public Set<ItemStack> getDeathDrops() {
